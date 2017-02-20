595d4cbd7a7e83af4f871f487b13fc35bd236ab5
Sam Cohen <samsam489@gmail.com>
2017-02-19 20:54:31 -0500

    Skeleton of 2048-game and computer AI.

---
+++ PlayerAI_3.py
@@ -0,0 +1,11 @@
+from BaseAI import BaseAI
+
+
+class PlayerAI(BaseAI):
+
+	def getMove(self, grid):
+		"""
+		returns an integer indicating the player's action.
+		0:"Up", 1:"Down", 2:"Left", 3:"Right"
+		"""
+		pass
